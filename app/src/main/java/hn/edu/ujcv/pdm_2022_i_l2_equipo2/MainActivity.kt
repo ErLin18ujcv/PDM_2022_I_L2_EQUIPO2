542b7f027252035fa6dfbebcb1af4d4362338dcb
erlin <erlinmejia98.emma@gmail.com>
2022-03-08 21:28:02 -0600

    Subiendo archivos de la apps

---
+++ app/src/main/java/hn/edu/ujcv/pdm_2022_i_l2_equipo2/MainActivity.kt
@@ -0,0 +1,11 @@
+package hn.edu.ujcv.pdm_2022_i_l2_equipo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
